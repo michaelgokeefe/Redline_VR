0dac876fd76d395c39fbe6ba9f67c9a10dee5909
Jesse Comb <jesse@northstargames.com>
2016-09-17 18:39:33 -0700

    motor curve

--- Assets/Scripts/Bike_Motor.cs
+++ Assets/Scripts/Bike_Motor.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class Bike_Motor : MonoBehaviour {
 
+    //public float CruiseSpeed;
+    public float CurrentVelocity;
+    //public float AccelerationTime;
+    public AnimationCurve AccelerationCurve;
+    public AnimationCurve DecelerationCurve;
+    public bool ThrottleHeld;
+
+    //public float PercentToCruiseSpeed;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +18,26 @@ public class Bike_Motor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKey(KeyCode.W)) {
+            //if (ThrottleHeld) {
+            //PercentToCruiseSpeed += Time.deltaTime / AccelerationTime;
+            //PercentToCruiseSpeed = Mathf.Clamp01(PercentToCruiseSpeed);
+
+            CurrentVelocity = CurrentVelocity + (AccelerationCurve.Evaluate(CurrentVelocity) * Time.deltaTime);
+
+        }
+        else {
+            if (CurrentVelocity > 0) {
+                CurrentVelocity = CurrentVelocity + (DecelerationCurve.Evaluate(CurrentVelocity) * Time.deltaTime);
+            }
+            else {
+                CurrentVelocity = 0;
+            }
+        }
+
+
 	
 	}
+
+    //public void 
 }
